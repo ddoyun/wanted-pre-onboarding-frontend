ebabe7abcecb1ddb89714ff0a5664cd431fb3539
ddoyun <soyoon4122@naver.com>
2023-08-09 23:07:55 +0900

    Feat: API 인스턴스 추가

---
+++ api/instance.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+// 토큰
+const accessToken = window.localStorage.getItem('access_token');
+
+export const instance = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+  headers: { 'Content-type': 'application/json' },
+});
+
+export const authInstance = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+  headers: {
+    Authorization: `Bearer ${accessToken}`,
+    'Content-type': 'application/json'
+  },
+});

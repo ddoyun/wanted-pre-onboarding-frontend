23b8632c8417cdf8b365976aef22eb3f2dae49a6
ddoyun <soyoon4122@naver.com>
2023-08-18 23:54:32 +0900

    Feat: ThemeProvider 전역 사용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import GlobalStyles from './style/globalStyle';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './styles/globalStyle';
+import Theme from './styles/theme';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Todo from './pages/Todo';
@@ -11,17 +13,19 @@ function App() {
   return (
     <BrowserRouter>
       <GlobalStyles />
-      <Routes>
-        <Route element={<PublicLayout />}>
-          <Route path="/" element={<SignIn />} />
-          <Route path="signin" element={<SignIn />} />
-          <Route path="signup" element={<SignUp />} />
-        </Route>
-        <Route element={<AuthLayout />}>
-          <Route path="/todo" element={<Todo />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <ThemeProvider theme={Theme}>
+        <Routes>
+          <Route element={<PublicLayout />}>
+            <Route path="/" element={<SignIn />} />
+            <Route path="signin" element={<SignIn />} />
+            <Route path="signup" element={<SignUp />} />
+          </Route>
+          <Route element={<AuthLayout />}>
+            <Route path="/todo" element={<Todo />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }

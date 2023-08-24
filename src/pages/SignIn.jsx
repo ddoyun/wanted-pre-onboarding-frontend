f07bde01182cc20438c629e1e02a47daa627da4e
ddoyun <soyoon4122@naver.com>
2023-08-24 23:45:42 +0900

    Fix: 호출시 이동 시점 변경

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,4 +1,4 @@
-import { React } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { signInApi } from '../api/api';
 import SignForm from '../components/sign/SignForm';
@@ -12,14 +12,14 @@ const SignIn = () => {
       const res = await signInApi(signInData);
       if (res.status === 200) {
         window.localStorage.setItem('access_token', res.data.access_token);
-        alert('로그인 성공!');
-        navigate('/todo');
       }
     } catch (error) {
       if (error.request.status === 404) {
         alert('해당 사용자가 존재하지 않습니다.');
       }
       console.error(error);
+    } finally {
+      navigate('/todo');
     }
   };
 
